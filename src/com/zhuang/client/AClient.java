5fc7f1a50ff5770756b67ee58af3a8af3a5e1c0b
zhuanglunhang <zhuanglunhang@jiuqi.com.cn>
2020-11-10 19:09:55 +0800

    使用maven

--- src/com/zhuang/client/AClient.java
+++
@@ -1,7 +0,0 @@
-package com.zhuang.client;
-
-public class AClient {
-	public static void main(String[] args) {
-		
-	}
-}
---
+++ src/main/java/com/talkzoom/ClientA.java
@@ -0,0 +1,27 @@
+package main.java.com.talkzoom;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * @description:
+ * @author: zhuanglunhang
+ * @create: 2020-11-10 18:50
+ */
+public class ClientA {
+
+  public static void main(String[] args) throws IOException {
+    Socket socket = new Socket("10.39.3.221", 65532);
+    OutputStream outputStream = socket.getOutputStream();
+    outputStream.write("你好".getBytes());
+    outputStream.close();
+//    InputStream inputStream = socket.getInputStream();
+//    byte[] data = new byte[1024];
+//    int len;
+//    while((len = inputStream.read(data, 0, 1024)) != 0) {
+//      System.out.println(new String(data, 0, len));
+//    }
+  }
+}
