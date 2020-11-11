6a24169a94ba1fc869ad48c12a32c1ac47ac273a
zhuanglunhang <zhuanglunhang@jiuqi.com.cn>
2020-11-11 10:25:59 +0800

    maven

---
+++ src/main/java/com/talkzoom/Client.java
@@ -0,0 +1,29 @@
+package com.talkzoom;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @description:
+ * @author: zhuanglunhang
+ * @create: 2020-11-10 18:40
+ */
+public class Client {
+
+  public static void main(String[] args) throws IOException {
+    ServerSocket serverSocket = new ServerSocket(65532);
+    System.out.println("等待连接");
+    Socket socket = serverSocket.accept();
+    System.out.println(socket.getInetAddress().getHostAddress() + "连接成功");
+    InputStream inputStream = socket.getInputStream();
+    byte[] data = new byte[1024];
+    int len;
+    StringBuilder stringBuilder = new StringBuilder();
+    while((len = inputStream.read(data, 0, 1024)) > 0) {
+      stringBuilder.append(new String(data, 0, len));
+    }
+    System.out.println(stringBuilder.toString());
+  }
+}
